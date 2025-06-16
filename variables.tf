1ed6973fe691b52c3230bc3539625ee2fd67859e
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-16 21:58:38 +0530

    updated .tf file

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "security_group_ids" {
+    default = ["sg-05847b88288dddafe"]
+}
+
+variable "tags" {
+    default = {
+        Name = "roboshop-cart"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
