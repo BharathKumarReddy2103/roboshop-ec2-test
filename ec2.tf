400264a5bb1e12d6bb0ba7e3d1f94c8657eebb42
BharathKumarReddy <nbkumar2103@gmail.com>
2025-08-01 14:09:17 +0530

    updated file

--- ec2.tf
+++ ec2.tf
@@ -2,5 +2,6 @@ module "ec2" {
     source = "../terraform-aws-instance"
     sg_ids = var.security_group_ids
     instance_type = var.instance_type
+    subnet_id     = var.subnet_id
     tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,9 @@ variable "tags" {
 variable "instance_type" {
     default = "t3.small"
 }
+
+variable "subnet_id" {
+  type        = string
+  default = "subnet-027e9b95abbffe496"
+  description = "subnet ID of bharath RHEL9"
+}
